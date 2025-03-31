e3129e7093fd2d82f3706523f9e0792449e6ab06
begenchmyrat <begenchmyratmeredamanov@gmail.com>
2025-03-31 18:54:30 +0500

    init

--- README.md
+++ README.md
@@ -1,5 +1,72 @@
-# Vue 3 + TypeScript + Vite
+# Vue Seven-Segment Display Component
 
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+A customizable seven-segment display component for Vue and TypeScript. Perfect for displaying numeric and alphanumeric values in a digital clock, scoreboard, or retro-style interface.
 
-Learn more about the recommended Project Setup and IDE Support in the [Vue Docs TypeScript Guide](https://vuejs.org/guide/typescript/overview.html#project-setup).
+
+| left-aligned (default)                             | right-aligned                                  |
+|----------------------------------------------------|------------------------------------------------|
+| ![Seven Segment Display start](./assets/start.jpg) | ![Seven Segment Display end](./assets/end.jpg) |
+
+![Seven Segment Display examples1](./assets/examples2.png)
+![Seven Segment Display examples](./assets/examples.png)
+
+## Installation
+
+You can install this package using npm or yarn:
+
+```sh
+npm install vue-ts-seven-segment-display
+```
+
+or
+
+```sh
+yarn add vue-ts-seven-segment-display
+```
+
+## Usage
+
+Import the component and use it in your Vue project:
+
+```tsx
+<template>
+  <SevenSegmentDisplay value="123" height="80" color="red" autoGrow />
+</template>
+<script setup lang="ts">
+import SevenSegmentDisplay from "vue-ts-seven-segment-display";
+</script>
+```
+
+## Props
+
+| Prop         | Type                       | Default   | Description |
+|-------------|----------------------------|-----------|-------------|
+| `value`     | `number \| string \| null` | `null` | The value to display. |
+| `height`    | `number \| string`         | `64`      | Height of the segment display. |
+| `segmentSize` | `number`                   | `8`       | Number of segments to display. |
+| `bgColor`   | `string`                   | `"#F2F2F2"` | Background color of inactive segments. |
+| `color`     | `string`                   | `"#545C6C"` | Color of active segments. |
+| `spacing`   | `number`                   | `8`       | Spacing between segments. |
+| `startFromEnd` | `boolean`                  | `false`   | Align numbers to the end of the display. |
+| `autoGrow`  | `boolean`                  | `false`   | Automatically adjust segment size based on input length. |
+
+## Supported Characters
+
+The following characters are supported for display:
+
+```
+0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z - _ = [ ]
+```
+
+## License
+
+This project is licensed under the ISC License.
+
+## Author
+
+Created by [begenchmyratmeredamanov](mailto:begenchmyratmeredamanov@gmail.com).
+
+## Repository
+
+[GitHub Repository](https://github.com/begenchmyrat/vue-ts-seven-segment-display)
+
--- src/components/SevenSegmentDisplay.vue
+++ src/components/SevenSegmentDisplay.vue
@@ -1,6 +1,147 @@
 <template>
-  <p>123</p>
+  <div :style="{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: `${spacing}px` }">
+    <svg
+      v-for="(segment, index) in segments"
+      :key="`segment-${index}`"
+      :height="height"
+      viewBox="0 0 64 104"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        v-for="(digitPosition, digitIndex) in digitPositions"
+        :key="`segment-digit-${digitIndex}`"
+        :d="digitPosition"
+        :fill="segment[digitIndex]"
+      />
+    </svg>
+  </div>
 </template>
 <script setup lang="ts">
+import {computed} from "vue";
+
+const { value, height, segmentSize : segmentSizeProp, color, bgColor, spacing, startFromEnd, autoGrow } = defineProps({
+  value: {
+    type: [Number, String, null],
+    default: null
+  },
+  height: {
+    type: [Number, String],
+    required: false,
+    default: 96
+  },
+  segmentSize: {
+    type: Number,
+    required: false,
+    default: 8
+  },
+  color: {
+    type: String,
+    required: false,
+    default: '#545C6C'
+  },
+  bgColor: {
+    type: String,
+    required: false,
+    default: '#F2F2F2'
+  },
+  spacing: {
+    type: [Number, String],
+    required: false,
+    default: 8
+  },
+  startFromEnd: Boolean,
+  autoGrow: Boolean,
+})
+
+const segmentSize = computed(() =>{
+  return autoGrow && value!==null && value.toString().length > segmentSizeProp ? value.toString().length : segmentSizeProp
+})
+
+const segments = computed(() => {
+  return Array.from({ length: segmentSize.value }, (_, i) => {
+    if (value === null) return digits["null"]
+    const valueString = value.toString().toUpperCase()
+
+    let startFromIndex= i < valueString.length ? i : null
+    let isNegative = valueString.charAt(0) === "-" && i === 0
+
+    if (startFromEnd) {
+      startFromIndex = i < (segmentSize.value - valueString.length)? null : i
+      isNegative= isNegative && startFromIndex === (segmentSize.value - valueString.length)
+    }
+
+    if (startFromIndex!== null) {
+      let digitIndex= startFromEnd? startFromIndex - (segmentSize.value - valueString.length) : i
+
+      if (isNegative) return digits["-"]
+
+      const digit = valueString.charAt(digitIndex)
+
+      if (digit in digits) return digits[digit as keyof typeof digits]
+
+      return digits["null"]
+    }
+
+    return digits["null"]
+  })
+})
+
+const digits = {
+  "null": [ bgColor, bgColor, bgColor, bgColor, bgColor, bgColor, bgColor ],
+  " ": [ bgColor, bgColor, bgColor, bgColor, bgColor, bgColor, bgColor ],
+  "-": [ bgColor, bgColor, bgColor, bgColor, bgColor, bgColor, color ],
+  "_": [ bgColor, bgColor, bgColor, color, bgColor, bgColor, bgColor ],
+  "=": [ bgColor, bgColor, bgColor, color, bgColor, bgColor, color ],
+  "[": [ color, bgColor, bgColor, color, color, color, bgColor ],
+  "]": [ color, color, color, color, bgColor, bgColor, bgColor ],
+  "0": [ color, color, color, color, color, color, bgColor ],
+  "1": [ bgColor, color, color, bgColor, bgColor, bgColor, bgColor ],
+  "2": [ color, color, bgColor, color, color, bgColor, color ],
+  "3": [ color, color, color, color, bgColor, bgColor, color ],
+  "4": [ bgColor, color, color, bgColor, bgColor, color, color ],
+  "5": [ color, bgColor, color, color, bgColor, color, color ],
+  "6": [ color, bgColor, color, color, color, color, color ],
+  "7": [ color, color, color, bgColor, bgColor, bgColor, bgColor ],
+  "8": [ color, color, color, color, color, color, color ],
+  "9": [ color, color, color, color, bgColor, color, color ],
+
+  "A":  [ color, color, color, bgColor, color, color, color ],
+  "B":  [ bgColor, bgColor, color, color, color, color, color ],
+  "C":  [ color, bgColor, bgColor, color, color, color, bgColor ],
+  "D":  [ bgColor, color, color, color, color, bgColor, color ],
+  "E":  [ color, bgColor, bgColor, color, color, color, color ],
+  "F":  [ color, bgColor, bgColor, bgColor, color, color, color ],
+  "G":  [ color, bgColor, color, color, color, color, bgColor ],
+  "H":  [ bgColor, bgColor, color, bgColor, color, color, color ],
+  "I":  [ bgColor, bgColor, bgColor, bgColor, color, color, bgColor ],
+  "J":  [ bgColor, color, color, color, color, bgColor, bgColor ],
+  "K":  [ color, bgColor, color, bgColor, color, color, color ],
+  "L":  [ bgColor, bgColor, bgColor, color, color, color, bgColor ],
+  "M":  [ color, bgColor, color, bgColor, color, bgColor, bgColor ],
+  "N":  [ color, color, color, bgColor, color, color, bgColor ],
+  "O":  [ color, color, color, color, color, color, bgColor ],
+  "P":  [ color, color, bgColor, bgColor, color, color, color ],
+  "Q":  [ color, color, color, bgColor, bgColor, color, color ],
+  "R":  [ color, color, bgColor, bgColor, color, color, bgColor ],
+  "S":  [ color, bgColor, color, color, bgColor, color, color ],
+  "T":  [ bgColor, bgColor, bgColor, color, color, color, color ],
+  "U":  [ bgColor, color, color, color, color, color, bgColor ],
+  "V":  [ bgColor, color, color, color, bgColor, color, bgColor ],
+  "W":  [ bgColor, color, bgColor, color, bgColor, color, bgColor ],
+  "X":  [ bgColor, color, color, bgColor, color, color, color ],
+  "Y":  [ bgColor, color, color, color, bgColor, color, color ],
+  "Z":  [ color, color, bgColor, color, bgColor, bgColor, color ]
+}
+
+const digitPositions = [
+  'M49 0H15L8 7L15 14H49L56 7L49 0Z',
+  'M64 51L64 15L57 8L50 15L50 44L57 51L64 51Z',
+  'M64 53L64 89L57 96L50 89L50 60L57 53L64 53Z',
+  'M49 90H15L8 97L15 104H49L56 97L49 90Z',
+  'M1.87959e-06 53L3.0598e-07 89L7 96L14 89L14 60L7 53L1.87959e-06 53Z',
+  'M1.87959e-06 51L3.0598e-07 15L7 8L14 15L14 44L7 51L1.87959e-06 51Z',
+  'M49 45H15L8 52L15 59H49L56 52L49 45Z'
+]
 
 </script>
