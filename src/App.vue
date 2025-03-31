c26162d959b8624f55dfc6b80a35b8c67546bdb5
begenchmyrat <begenchmyratmeredamanov@gmail.com>
2025-03-31 18:23:39 +0500

    clear

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <SevenSegmentDisplay />
+</template>
+<script setup lang="ts">
+import SevenSegmentDisplay from './components/SevenSegmentDisplay.vue'
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from "path"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+
+  build: {
+    cssCodeSplit: true,
+    target: "esnext",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "SevenSegmentDisplay",
+      fileName: (format) => `vue-ts-seven-segment-display.${format}.js`,
+    },
+
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+})
