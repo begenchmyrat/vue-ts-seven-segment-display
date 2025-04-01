18db10a3c2c3d38f3432e8d3c494737506c715d0
begenchmyrat <begenchmyratmeredamanov@gmail.com>
2025-04-01 10:47:55 +0500

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from "node:path";
-import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue()],
 
   build: {
     lib: {
